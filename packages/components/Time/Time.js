b1664cfbaa654528a4bd78669b24ac953e0f50d8
Ryan Atkinson <ryan@echobind.com>
2019-03-18 17:04:40 -0400

    timezones working!

--- packages/components/Time/Time.js
+++ packages/components/Time/Time.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import moment from 'moment';
+import moment from 'moment-timezone';
 import { Text, View, StyleSheet } from 'react-native';
 
 const TIME_FORMAT = 'h:mm a';
+const TIME_ZONE_MAP = {
+  EST: 'America/New_York',
+  CST: 'America/Chicago',
+  MST: 'America/Denver',
+  PST: 'America/Los_Angeles',
+};
 
 const Time = ({ timezone }) => {
-  // EST, CST, MST, PST
   const getCurrentTime = (timezone = null) => {
-    if (!timezone) {
+    // do not format an invalid or missing timezone
+    if (!timezone || !Object.keys(TIME_ZONE_MAP).includes(timezone)) {
       return moment().format(TIME_FORMAT);
     }
-  
-    // TODO make timezones work :)
-    return moment().tz(timezone).format(TIME_FORMAT);
+    return moment().tz(TIME_ZONE_MAP[timezone]).format(TIME_FORMAT);
   };
 
   const time = getCurrentTime(timezone);
